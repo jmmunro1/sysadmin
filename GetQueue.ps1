5617283cc5156c42944609e9b725ecc910c54760
jmmunro1 <42035313+jmmunro1@users.noreply.github.com>
2018-10-25 13:13:39 -0400

    Update GetQueue.ps1

--- GetQueue.ps1
+++ GetQueue.ps1
@@ -1,5 +1,5 @@
 ﻿$UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://lincolnmail.lbscares.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
 Import-PSSession $Session
-Get-Queue | Select Identity, Status, MessageCount | export-csv "\\filedomain\data\IT Dept\queue\queue$(get-date -f yyyyMMdd).csv"
+Get-Queue | Select Identity, Status, MessageCount | export-csv "\\FileLocation$(get-date -f yyyyMMdd).csv"
 Read-Host -Prompt "Press Enter to exit"
