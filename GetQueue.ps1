d461bc0940c89c359f7ab5c50498fd3c329b7bd3
jmmunro1 <42035313+jmmunro1@users.noreply.github.com>
2022-04-07 13:28:11 -0400

    Delete GetQueue.ps1

--- GetQueue.ps1
+++
@@ -1,5 +0,0 @@
-﻿$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://lincolnmail.lbscares.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session
-Get-Queue | Select Identity, Status, MessageCount | export-csv "\\FileLocation$(get-date -f yyyyMMdd).csv"
-Read-Host -Prompt "Press Enter to exit"
